3896ea5dca3eef0c13fb7a4ee59ecbe1c4f0b65f
linhan <linhan@3dbox.com>
2022-06-08 14:03:48 +0800

    update demo

--- filter_image/app.py
+++ filter_image/app.py
@@ -1,14 +1,30 @@
 #-*- coding: UTF-8 -*-
 from PIL import Image
+from flask import Flask, make_response, render_template
 import os
+import random
 
-filterSize = (300, 300)
-filterPath = './image/'
+filterSize = (278, 192)
+filterPath = os.path.split(__file__)[0] + '/images/'
+filterFiles = []
+
+app = Flask(__name__)
+
+@app.route('/images/<name>',methods = ['POST', 'GET'])
+def images(name):
+    path = filterPath + name
+    if ~os.path.exists(path):
+        name = filterFiles[random.randint(0, filterFiles.__len__() - 1)]
+    image = open(filterPath + name, 'rb').read()
+    response = make_response(image)
+    response.headers['Content-Type'] = 'image/jpg'
+    return response
+
 
 # 遍历指定目录，显示目录下的所有文件名
 def filterFile(filepath):
-    pathDir =  os.listdir(filepath)
-    for allDir in pathDir:
+    dirs =  os.listdir(filepath)
+    for allDir in dirs:
         path = os.path.join('%s/%s' % (filepath, allDir))
         image = Image.open(path)
         if image.size > filterSize:
@@ -16,8 +32,10 @@ def filterFile(filepath):
             y = 0
             img = image.crop((x, y, x + filterSize[0], y + filterSize[1]))
             img.save(filterPath + allDir)
+    return dirs
 
+filterFiles.extend(filterFile('./排球少年'))
+filterFiles.extend(filterFile('./排球少年2'))
+filterFiles.extend(filterFile('./排球少年3'))
 
-filterFile('./排球少年')
-# filterFile('./排球少年2')
-# filterFile('./排球少年3')
+app.run(host="0.0.0.0", port=8080)
--- watermark_pdf/app.py
+++ watermark_pdf/app.py
@@ -1,8 +1,9 @@
 from PyPDF2 import PdfFileReader, PdfFileWriter
-from PyQt6.QtCore import *
-from PyQt6.QtGui import *
-from PyQt6.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
 import sys
+import os
 
 def add_watermark(pdf_file_in, pdf_file_mark, pdf_file_out):
     """把水印添加到pdf中"""
@@ -23,9 +24,7 @@ def add_watermark(pdf_file_in, pdf_file_mark, pdf_file_out):
         pdf_output.addPage(page)
     pdf_output.write(open(pdf_file_out, 'wb'))
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-
+def start_watermark():
     files = QFileDialog.getOpenFileNames()[0]
     markfile = QFileDialog.getOpenFileName()[0]
 
@@ -34,4 +33,58 @@ if __name__ == '__main__':
         pdf_file_mark = markfile
         pdf_file_out = file.replace('.pdf', '_Marked.pdf')
         add_watermark(pdf_file_in, pdf_file_mark, pdf_file_out)
-    
+
+
+def button1_click(line1):
+    dir = QFileDialog.getExistingDirectory()
+    line1.setText(dir)
+
+
+def button2_click(line2):
+    file = QFileDialog.getOpenFileName()[0]
+    line2.setText(file)
+
+def button3_click(line1, line2):
+    dir = line1.text()
+    markfile = line2.text()
+    files =  os.listdir(dir)
+    for file in files:
+        path = os.path.join('%s/%s' % (dir, file))
+
+        file_out = path.replace('.pdf', '_Marked.pdf')
+        add_watermark(path, markfile, file_out)
+    QMessageBox.about(None, '提示', '水印添加完成')
+
+
+
+app = QApplication(sys.argv)
+
+win = QWidget()
+win.setWindowTitle('pdf批量添加水印')
+win.setMinimumWidth(400)
+
+line1 = QLineEdit()
+line2 = QLineEdit()
+
+button1 = QPushButton('预览')
+button2 = QPushButton('预览')
+button3 = QPushButton('开始')
+
+button1.clicked.connect(lambda: button1_click(line1))
+button2.clicked.connect(lambda: button2_click(line2))
+button3.clicked.connect(lambda: button3_click(line1, line2))
+
+layout = QGridLayout()
+layout.addWidget(QLabel('批量pdf路径'), 0, 0)
+layout.addWidget(line1, 0, 1)
+layout.addWidget(button1, 0, 2)
+
+layout.addWidget(QLabel('参考pdf水印'), 1, 0)
+layout.addWidget(line2, 1, 1)
+layout.addWidget(button2, 1, 2)
+
+layout.addWidget(button3, 3, 0, 1, 3)
+
+win.setLayout(layout)
+win.show()
+sys.exit(app.exec())
